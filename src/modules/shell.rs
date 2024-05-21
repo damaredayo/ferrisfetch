9b198844c165e8317dba489583bfadd2e6649316
Tyler Thompson <yoitscore@gmail.com>
2024-05-21 07:42:40 +0100

    feat: add get_shell_info for mac + windows

--- src/modules/shell.rs
+++ src/modules/shell.rs
@@ -14,3 +14,21 @@ fn get_shell_info() -> Result<String, String> {
 
     Err("Unable to detect shell on Linux".to_string())
 }
+
+#[cfg(target_os = "macos")]
+fn get_shell_info() -> Result<String, String> {
+    if let Ok(shell) = std::env::var("SHELL") {
+        return Ok(shell.split('/').last().unwrap_or(&shell).to_string());
+    }
+
+    Err("Unable to detect shell on macOS".to_string())
+}
+
+#[cfg(target_os = "windows")]
+fn get_shell_info() -> Result<String, String> {
+    if let Ok(shell) = std::env::var("ComSpec") {
+        return Ok(shell.split('\\').last().unwrap_or(&shell).to_string());
+    }
+
+    Err("Unable to detect shell on Windows".to_string())
+}
