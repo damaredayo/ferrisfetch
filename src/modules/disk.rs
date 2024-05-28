918b40ff8a3a401a23df22208540a5636cd17798
Tyler Thompson <yoitscore@gmail.com>
2024-05-28 23:34:57 +0100

    feat: add disk support

--- src/modules/disk.rs
+++ src/modules/disk.rs
@@ -1,7 +1,15 @@
 use crate::State;
 
 impl State {
-    pub fn fetch_disk(&self) -> String {
-        format!("")
+    pub fn fetch_disk(&self) -> Vec<String> {
+        sysinfo::Disks::new_with_refreshed_list().iter().map(|disk| {
+            format!(
+                "{}: {:.2}GB / {:.2}GB ({})",
+                disk.mount_point().to_str().unwrap_or(""),
+                disk.total_space() as f64 / 1024.0 / 1024.0 / 1024.0,
+                disk.available_space() as f64 / 1024.0 / 1024.0 / 1024.0,
+                disk.file_system().to_str().unwrap_or(""),
+            )
+        }).collect()
     }
 }
